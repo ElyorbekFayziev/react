6fe78ea8caded725bae7c3abe08187f7bdae49fa
Elyor <abduganifayziev@gmail.con>
2022-11-11 14:47:25 +0500

    beshinchi dars

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './index.css'
-
 let students = [
   {id:1,name:"Elyorbek Fayziev",age:16,address:"Samarqand",status:"Student",nickname:"deWebON",univ:"...",job:"Developer"},
   {id:2,name:"Elyorbek Fayziev",age:16,address:"Samarqand",status:"Student",nickname:"deWebON",univ:"...",job:"Developer"},
@@ -12,14 +11,25 @@ let students = [
     constructor(props){
       super(props);
       this.state = {
-        data:students
+        data:students,
+        name:"",
+        age:"",
+        address:"",
+        status:"",
+        nickname:"",
+        univ:"",
+        job:""
       }
       this.setState({data:students})
     }
       render(){
+        const onChange =(e)=>{
+          this.setState({[e.target.name]:e.target.value})
+        }
         const id =()=>{
           return this.state.data.map((v)=>{
             return(
+              <tbody>
               <tr>
             <td>{v.id}</td>
             <td>{v.name}</td>
@@ -29,35 +39,29 @@ let students = [
             <td>{v.nickname}</td>
             <td>{v.univ}</td>
             <td>{v.job}</td>
-            <td>delete</td>
-            <td>edit</td>
+            <td><button onClick={()=>delet(v.id)}>delete</button></td>
+            <td><button onClick={()=>edit(v.id)}>edit</button></td>
           </tr>
+          </tbody>
             )
           })
         }
-        // const delet =(id)=>{
-        //   let ar = this.state.data.filter((v)=>id!==v.id)
-        //   this.state.data = ar
-        //   console.log(this.state.data);
-        // }
+        const push = ()=>{
+          return (
+            this.state.data.push({id:this.state.data.length+1,name:this.state.name,age:this.state.age,address:this.state.address,status:this.state.status,nickname:this.state.nickname,univ:this.state.univ,job:this.state.job},()=>{this.setState({data:students})})
+          )
+        }
+        const delet =(id)=>{
+          let ar = this.state.data.filter((v)=>v.id!==id)
+          this.setState({data:ar})
+        }
         const edit = (id)=>{
-          return this.state.data.map((v)=>{
-            if(v.id == id){
-            return(
-              <input value={v.id}></input>,
-              <input value={v.name}></input>,
-              <input value={v.age}></input>,
-              <input value={v.address}></input>,
-              <input value={v.status}></input>,
-              <input value={v.nickname}></input>,
-              <input value={v.univ}></input>,
-              <input value={v.job}></input>
-            )
-            }
-          })
+
         }
         return(
+          <div>
           <table className='table'>
+            <thead>
             <tr>
               <th>ID</th>
               <th>NAME</th>
@@ -70,8 +74,21 @@ let students = [
               <th>Delete</th>
               <th>Edit</th>
             </tr>
+            </thead>
             {id()}
           </table>
+          <div>
+              <input value={this.state.name} name='name' onChange={onChange} type="text" placeholder='name'/>
+              <input value={this.state.age} name='age' onChange={onChange} type="text" placeholder='age'/>
+              <input value={this.state.address} name='address' onChange={onChange} type="text"  placeholder='address'/>
+              <input value={this.state.status} name='status' onChange={onChange} type="text" placeholder='status'/>
+              <input value={this.state.nickname} name='nickname' onChange={onChange} type="text" placeholder='nickname'/>
+              <input value={this.state.univ} name='univ' onChange={onChange} type="text" placeholder='univ'/>
+              <input value={this.state.job} name='job' onChange={onChange} type="text" placeholder='job'/>
+              <button onClick={push}></button>
+              <button onClick={edit}></button>
+            </div>
+          </div>
         )
   }
 }
